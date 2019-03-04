34095d16147af582594d6809dc2494b7badc8601
xxonbang <mackulri@gmail.com>
2019-03-04 23:57:52 +0900

    delete dummy codes

--- car-mng-app/src/app/app.component.ts
+++ car-mng-app/src/app/app.component.ts
@@ -7,37 +7,16 @@ import { ChangeDetectorRef } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  // activateClass(subMenu){
-  //   subMenu.active = !subMenu.active;    
-  // }
 
   public curUrl;
-  public ref;
   public _router;
 
-  constructor(private changeDetector: ChangeDetectorRef, private router: Router) {
+  constructor(private router: Router) {
     this._router = router;
-    this.ref = changeDetector;
-
-
-    // this.ref.detectChanges();
   }
 
   ngOnInit() {
     console.log(this._router.url);
-    //this.curUrl = this.router.url;
   }
 
-  clickFunc() {
-    console.log(this._router.url);
-    //this.curUrl = this.router.url;
-  }
-
-  // constructor() {
-  //   this.subMenus = [
-  //     {name: '사용내역 입력'},
-  //     {name: '사용내역 검색'},
-  //     {name: '차량관리'},
-  //   ];    
-  // }
 }
