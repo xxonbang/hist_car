94ffb738b95c7ca09e36d4c918baa6343eab677a
Byeongcheol Son <mackulri@gmail.com>
2019-04-11 10:38:38 +0900

    car-use-hist 서버로 부터 사용내역 data 받아와서 화면에 list grid 그리는 것 구현 완료  //  관련하여 불필요 로직 정리 및 주석처리 추가  //  향후 post 로 서버에 내용 밀어넣는 기능 및 검색조건 설정하여 검색하는 기능 추가 예정

--- car-mng-app/src/app/history-search/history-search.component.ts
+++ car-mng-app/src/app/history-search/history-search.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, MatTableDataSource } from '@angular/material';
-import { DataSource } from '@angular/cdk/collections';
-import { Observable } from 'rxjs';
 import { SelectionModel } from '@angular/cdk/collections';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 import { InputFieldsModel } from '../input-fields-model';
 import { RecordService } from '../record.service'
@@ -29,67 +27,64 @@ export class HistorySearchComponent implements OnInit {
 
   // 모든 input box 에 대한 입력 값들을 받게 될 record 변수를 InputFieldsModel 타입으로 생성
   record = new InputFieldsModel();
+
+  // 검색 결과를 담을 recordList 변수를 inputFieldsModel 타입의 배열로 생성
   recordList: InputFieldsModel[];
 
-  // TODO : 서버로부터 data를 받아와서 arrary 방식으로 데이터 넣는 코딩 필요
-  TABLE_DATA: InputFieldsModel[] = [
-
-    // { dateFrom: new Date(), dateTo: new Date(), driverDept: 'APP서비스그룹', driverNm: '손병철', useType: '업무용', usePurs: '회의 참석', usePursDetail: '발주처 워크샵 참여', driveDist: 5, accumMileage: 2000, dest: '속초', dropby: '명동', fueling: 60, histCar: '' },
-
-  ];
-
   // table 의 header 부분 컬럼들 셋팅
-  displayedColumns: string[] = ['select', 'seq', 'dateFrom', 'dateTo', 'driverDept', 'driverNm', 'useType', 'usePurs', 'usePursDetail', 'driveDist', 'accumMileage', 'dest', 'dropby', 'fueling', 'carid'];
+  displayedColumns: string[] = ['select', 'seq', 'datefrom', 'dateto', 'driverdept', 'drivernm', 'usetype', 'usepurs', 'usepursdetail', 'drivedist', 'accummileage', 'dest', 'dropby', 'fueling', 'carid'];
   // table 의 data 들에 대한 source
-  dataSource = new MatTableDataSource(this.TABLE_DATA);
+  dataSource = new MatTableDataSource(this.recordList);
   // 체크박스용
   selection = new SelectionModel<InputFieldsModel>(true, []);
 
 
   constructor(
+    // 날짜를 yyyy-mm-dd 형태로 구현해 주기 위한 DatePipe 생성
     private datePipe: DatePipe,
+    // RecordService 사용을 위한 sevice 생성
     private service: RecordService
   ) { }
 
   // pagination
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
+
   ngOnInit() {
 
+    // 페이지 loading 시 차량목록을 drop-box 에 바로 loading 해주기 위하여 ngOnInit에 포함
     this.getHistCarList();
 
+    // table 의 paginator 기능을 위한 선언
     this.dataSource.paginator = this.paginator;
-    // this.record.dateFrom = new Date();
-    // this.record.dateTo = new Date();
 
+    // page 내 input box 들에 대한 Form 선언
     this.historyInputForm = new FormGroup({
       dateFrom: new FormControl(this.record.datefrom, [Validators.required]),
       dateTo: new FormControl(),
       histCar: new FormControl()
     });
 
+    // 사용 일자 input box 내 default 로 오늘 날짜를 기입하기 위한 기능
     const today = new Date();
     this.historyInputForm.controls['dateFrom'].setValue(today);
     this.historyInputForm.controls['dateTo'].setValue(today);
 
-    console.dir('### ngOnInit is working!!');
-    this.setSubscribe();
   }
 
-  setSubscribe() {
+  // setSubscribe() {
 
-    this.service.carUseHist$.pipe().subscribe(res => {
-      console.dir('### setSubscribe is working!!');
-      // res
-      this.dataSource.data = res;
+  //   this.service.carUseHist$.pipe().subscribe(res => {
+  //     console.dir('### setSubscribe is working!!');
+  //     // res
+  //     this.dataSource.data = res;
 
-      // this.TABLE_DATA = res;
-    });
-  }
+  //     // this.TABLE_DATA = res;
+  //   });
+  // }
 
   // 차량선택 input box 내 List 조회용
   getHistCarList() {
-    // const that = this;
     this.service.getHistCarSelectionList()
       .subscribe(
         this.getHistCarListOk(),
@@ -99,18 +94,12 @@ export class HistorySearchComponent implements OnInit {
 
   // 서버로 부터 받아온 response SelectionListModel 배열에 담아 html 에서 사용할 수 있도록 함.
   getHistCarListOk() {
-    return (res => {
-      //hat.TABLE_DATA = res;
-      res = [{ dateFrom: new Date(), dateTo: new Date(), driverDept: 'APP서비스그룹', driverNm: '손병철', useType: '업무용', usePurs: '회의 참석', usePursDetail: '발주처 워크샵 참여', driveDist: 5, accumMileage: 2000, dest: '속초', dropby: '명동', fueling: 60, histCar: '' }];
-      this.service.carUseHist$.emit(res);
-    });
+    return (res: SelectionListModel[]) => this.histCarList = res;
   }
 
   // 서버 통신 시, error 처리
   getHistCarListError() {
-    return (error => {
-      console.log(error);
-    });
+    return (error) => console.log(error);
   }
 
   // server 와의 통신을 위해 date format 변경
@@ -140,6 +129,7 @@ export class HistorySearchComponent implements OnInit {
     return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.drivernm + 1}`;
   }
 
+  // search 버튼 클릭 시, service의 getRecord()를 호출 -> res 값을 받아와 OK 와 Err 로 전달
   search() {
     this.service.getRecord()
       .subscribe(
@@ -148,30 +138,14 @@ export class HistorySearchComponent implements OnInit {
       )
   }
 
+  // search 기능 OK 시 res 값을 dataSource 에 넣어줌
   searchRecordListOK() {
-    return (res => {
-      // this.recordList = res;
-      //res = { dateFrom: new Date(), dateTo: new Date(), driverDept: 'APP서비스그룹', driverNm: '손병철', useType: '업무용', usePurs: '회의 참석', usePursDetail: '발주처 워크샵 참여', driveDist: 5, accumMileage: 2000, dest: '속초', dropby: '명동', fueling: 60, histCar: '' };
-
-      this.dataSource.data = res;
-      // this.service.carUseHist$.emit(res);
-    });
+    return (res) => this.dataSource.data = res;
   }
 
+  // search 기능 err 수행
   searchRecordListErr() {
-    return (error => {
-      console.log(error);
-    });
+    return (error) => console.log(error);
   }
-  // export class RecordDataSource extends DataSource<any> {
-  //   constructor(private service: RecordService) {
-  //     super();
-  //   }
-  //   connect(): Observable<InputFieldsModel[]> {
-  //     return this.service.getRecord();
-  //   }
-  //   disconnect() { }
-  // }
-
 
 }
